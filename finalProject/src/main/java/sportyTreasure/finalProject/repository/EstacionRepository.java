52d89898fc6a507aea60e8143dedd1425f87baa8
nicolebernal <nicolemichelle120@gmail.com>
2023-11-05 12:46:05 -0500

    Agregando repositories y conexion a base de datos

---
+++ finalProject/src/main/java/sportyTreasure/finalProject/repository/EstacionRepository.java
@@ -0,0 +1,7 @@
+package sportyTreasure.finalProject.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import sportyTreasure.finalProject.entity.Estacion;
+
+public interface EstacionRepository extends JpaRepository<Estacion,Long> {
+}
---
+++ finalProject/src/main/java/sportyTreasure/finalProject/repository/JuagdorRepository.java
@@ -0,0 +1,7 @@
+package sportyTreasure.finalProject.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import sportyTreasure.finalProject.entity.Jugador;
+
+public interface JuagdorRepository extends JpaRepository<Jugador,Long> {
+}
